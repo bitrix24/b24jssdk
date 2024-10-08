883bf77f6797af3777d19b0dcd32bf3bd978bb89
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-10-08 15:33:56 +0300

    add iban test page

--- playgrounds/jssdk/pages/index.vue
+++ playgrounds/jssdk/pages/index.vue
@@ -11,6 +11,11 @@ definePageMeta({
 		<li class="py-2"><NuxtLink class="hover:text-info" to="/hook/testing-rest-api-calls">Testing Rest-Api Calls</NuxtLink></li>
 	</ul>
 	
+	<h2 class="text-h2">Frame</h2>
+	<ul class="list-disc list-inside">
+		<li class="py-2"><NuxtLink class="hover:text-info" to="/frame/testing-rest-api-calls">Testing Rest-Api Calls</NuxtLink></li>
+	</ul>
+	
 	<h2 class="mt-3 text-h2">Core</h2>
 	<ul class="list-disc list-inside">
 		<li class="py-2"><NuxtLink class="hover:text-info" to="/core/use-result">Result</NuxtLink></li>
@@ -19,5 +24,6 @@ definePageMeta({
 	<h2 class="mt-3 text-h2">Tools</h2>
 	<ul class="list-disc list-inside">
 		<li class="py-2"><NuxtLink class="hover:text-info" to="/tools/use-logger">LoggerBrowser</NuxtLink></li>
+		<li class="py-2"><NuxtLink class="hover:text-info" to="/tools/iban">IBAN</NuxtLink></li>
 	</ul>
 </template>
---
+++ playgrounds/jssdk/pages/tools/iban.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import useFormatter from "@bitrix24/b24jssdk/tools/useFormatters";
+
+definePageMeta({
+	layout: "page"
+})
+
+const { formatterIban } = useFormatter('en-US')
+</script>
+
+<template>
+	<Info><strong>IBAN</strong>
+		<br>{{ formatterIban.printFormat('BY09ALFA30132120160130270000', ' ') }}
+		<br>{{ formatterIban.printFormat('RU0204452560040702810412345678901', ' ') }}
+		<br>{{ formatterIban.printFormat('IR710570029971601460641001', ' ') }}
+		<br>{{ formatterIban.printFormat('AZ77VTBA00000000001234567890', ' ') }}
+		<br>{{ formatterIban.printFormat('GE60NB0000000123456789', ' ') }}
+		<br>{{ formatterIban.printFormat('JO71CBJO0000000000001234567890', ' ') }}
+	</Info>
+</template>

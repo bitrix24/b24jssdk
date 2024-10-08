1be22a240598716fa54688502f909d5fad427d2a
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-10-08 15:34:31 +0300

    add app layout

--- playgrounds/jssdk/layouts/app.vue
+++ playgrounds/jssdk/layouts/app.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 useHead({
 	bodyAttrs: {
-		class: 'bg-white font-b24-system text-gray-900 antialiased'
+		class: 'bg-white font-b24-system text-gray-900 antialiased px-2'
 	}
 })
 </script>

910df7774632769a7d3a17b700ecc3dd3944aafb
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-10-09 17:50:04 +0300

    fix config

--- playgrounds/jssdk/nuxt.config.ts
+++ playgrounds/jssdk/nuxt.config.ts
@@ -1,7 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {readFileSync} from "fs";
+
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: {enabled: false},
+	devServer: {
+		port: 3000,
+		/*/
+		host: 'custom.mydomain.local',
+		https: {
+			key: '.../source/ssl/custom.mydomain.local-key.pem',
+			cert: '.../source/ssl/custom.mydomain.local.pem'
+		},
+		//*/
+		loadingTemplate: () =>
+		{
+			return readFileSync('./template/devServer-loading.html', 'utf-8');
+		}
+	},
 	css: ['~/assets/css/main.css'],
 	postcss: {
 		plugins: {

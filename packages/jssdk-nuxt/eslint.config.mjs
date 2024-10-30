76d6f97cd063ee81de67660bf2ff1ba9f18b2c13
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-10-30 12:24:12 +0300

    add new ver for nuxt Plugin

--- packages/jssdk-nuxt/eslint.config.mjs
+++ packages/jssdk-nuxt/eslint.config.mjs
@@ -3,18 +3,18 @@ import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
 
 // Run `npx @eslint/config-inspector` to inspect the resolved config interactively
 export default createConfigForNuxt({
-	features: {
-		// Rules for module authors
-		tooling: true,
-		// Rules for formatting
-		stylistic: true,
-	},
-	dirs: {
-		src: [
-			'./playground',
-		],
-	},
+  features: {
+    // Rules for module authors
+    tooling: true,
+    // Rules for formatting
+    stylistic: true,
+  },
+  dirs: {
+    src: [
+      './playground',
+    ],
+  },
 })
-.append(
-  // your custom flat config here...
-)
+  .append(
+    // your custom flat config here...
+  )
--- packages/jssdk-nuxt/src/module.ts
+++ packages/jssdk-nuxt/src/module.ts
@@ -1,57 +1,22 @@
-import {defu} from 'defu'
-import {defineNuxtModule, addPlugin, useNuxt, createResolver} from '@nuxt/kit'
-import logger, { LogLevels } from './logger/server'
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
+// Module options TypeScript interface definition
 export interface ModuleOptions
 {
-	quiet: boolean
 }
 
-const defaults = (nuxt = useNuxt()): ModuleOptions => ({
-	quiet: nuxt.options.logLevel === 'silent'
-})
-
-/**
- * Module for communication with B24
- */
 export default defineNuxtModule<ModuleOptions>({
 	meta: {
-		/**
-		 * @todo fix this
-		 */
-		name: '@bitrix24/b24jssdk-nuxt',
-		version: '0.1.0',
-		configKey: 'b24JsSdk',
-		compatibility: {
-			nuxt: '>=3.13.0'
-		}
+		name: 'my-module',
+		configKey: 'myModule',
 	},
-	defaults,
+	// Default configuration options of the Nuxt module
+	defaults: {},
 	setup(_options, _nuxt)
 	{
-		if(_options.quiet)
-		{
-			logger.level = LogLevels.silent;
-		}
+		const resolver = createResolver(import.meta.url)
 		
-		// Default runtimeConfig ////
-		_nuxt.options.runtimeConfig.public.b24Frame = defu(
-			_nuxt.options.runtimeConfig.public?.b24Frame || {},
-			_options
-		);
-		
-		/**
-		 * @todo fix this
-		 */
-		logger.withTag('setup').start('Start @bitrix24/b24jssdk-nuxt ...');
-		
-		const resolver = createResolver( import.meta.url );
-		
-		// Plugins ////
-		addPlugin({
-			src: resolver.resolve('./runtime/plugin')
-		});
-		
-		logger.withTag('setup').success('plugin');
+		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+		addPlugin(resolver.resolve('./runtime/plugin'))
 	},
 })

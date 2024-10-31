bcdf1336c7485f296bf453be1da9a43618e1d855
Shevchik Igor <s.hevchik.mail@gmail.com>
2024-10-31 11:03:44 +0300

    add @bitrix24/b24jssdk-nuxt

--- packages/jssdk-nuxt/src/module.ts
+++ packages/jssdk-nuxt/src/module.ts
@@ -1,22 +1,25 @@
 import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
-// Module options TypeScript interface definition
 export interface ModuleOptions
 {
 }
 
 export default defineNuxtModule<ModuleOptions>({
 	meta: {
-		name: 'my-module',
-		configKey: 'myModule',
+		name: '@bitrix24/b24jssdk-nuxt',
+		version: '0.1.0',
+		configKey: 'B24JsSdkNuxt',
+		compatibility: {
+			nuxt: '>=3.13.0'
+		}
 	},
-	// Default configuration options of the Nuxt module
 	defaults: {},
 	setup(_options, _nuxt)
 	{
 		const resolver = createResolver(import.meta.url)
 		
-		// Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-		addPlugin(resolver.resolve('./runtime/plugin'))
+		addPlugin({
+			src: resolver.resolve('./runtime/plugin')
+		})
 	},
 })

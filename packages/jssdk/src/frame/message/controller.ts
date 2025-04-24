c7ef7a100a84490620100d7a4abe33e21480baff
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-04-24 13:29:29 +0300

    fix(placement.bindEvent): add callBack

--- packages/jssdk/src/frame/message/controller.ts
+++ packages/jssdk/src/frame/message/controller.ts
@@ -1,201 +1,216 @@
 import { LoggerBrowser, LoggerType } from '../../logger/browser'
-
 import { MessageCommands } from './commands'
 import { AppFrame } from '../frame'
-
 import Text from '../../tools/text'
+import { omit } from '../../tools'
 
 interface PromiseHandlers {
-	resolve: (value: any) => void
-	reject: (reason?: any) => void
-	timeoutId: any
+  resolve: (value: any) => void
+  reject: (reason?: any) => void
+  timeoutId: any
 }
 
 /**
  * Parent Window Request Parameters
  * @prop isSafely auto completion mode Promise.resolve()
  * @prop safelyTime after what time (900 ms) should it be automatically resolved Promise
+ * @prop callBack for placement event
  */
 export interface SendParams {
-	[index: string]: any
+  [index: string]: any
 
-	isSafely?: boolean
-	safelyTime?: number
+  isSafely?: boolean
+  safelyTime?: number
+  callBack?: (...args: any[]) => void
 }
 
 /**
  * Parent Window Communication Manager at B24
  */
 export class MessageManager {
-	#appFrame: AppFrame
-	#callbackPromises: Map<string, PromiseHandlers>
-	protected _logger: null | LoggerBrowser = null
-	private runCallbackHandler: OmitThisParameter<(event: MessageEvent) => void>
+  #appFrame: AppFrame
+  #callbackPromises: Map<string, PromiseHandlers>
+  #callbackSingletone: Map<string, (...args: any[]) => void>
+  protected _logger: null | LoggerBrowser = null
+  private readonly runCallbackHandler: OmitThisParameter<(event: MessageEvent) => void>
 
-	constructor(appFrame: AppFrame) {
-		this.#appFrame = appFrame
+  constructor(appFrame: AppFrame) {
+    this.#appFrame = appFrame
 
-		this.#callbackPromises = new Map()
+    this.#callbackPromises = new Map()
+    this.#callbackSingletone = new Map()
 
-		this.runCallbackHandler = this._runCallback.bind(this)
-	}
+    this.runCallbackHandler = this._runCallback.bind(this)
+  }
 
-	setLogger(logger: LoggerBrowser): void {
-		this._logger = logger
-	}
+  setLogger(logger: LoggerBrowser): void {
+    this._logger = logger
+  }
 
-	getLogger(): LoggerBrowser {
-		if (null === this._logger) {
-			this._logger = LoggerBrowser.build(`NullLogger`)
+  getLogger(): LoggerBrowser {
+    if (null === this._logger) {
+      this._logger = LoggerBrowser.build(`NullLogger`)
 
-			this._logger.setConfig({
-				[LoggerType.desktop]: false,
-				[LoggerType.log]: false,
-				[LoggerType.info]: false,
-				[LoggerType.warn]: false,
-				[LoggerType.error]: true,
-				[LoggerType.trace]: false,
-			})
-		}
+      this._logger.setConfig({
+        [LoggerType.desktop]: false,
+        [LoggerType.log]: false,
+        [LoggerType.info]: false,
+        [LoggerType.warn]: false,
+        [LoggerType.error]: true,
+        [LoggerType.trace]: false,
+      })
+    }
 
-		return this._logger
-	}
+    return this._logger
+  }
 
-	// region Events ////
-	/**
-	 * Subscribe to the onMessage event of the parent window
-	 */
-	subscribe() {
-		window.addEventListener('message', this.runCallbackHandler)
-	}
+  // region Events ////
+  /**
+   * Subscribe to the onMessage event of the parent window
+   */
+  subscribe() {
+    window.addEventListener('message', this.runCallbackHandler)
+  }
 
-	/**
-	 * Unsubscribe from the onMessage event of the parent window
-	 */
-	unsubscribe(): void {
-		window.removeEventListener('message', this.runCallbackHandler)
-	}
-	// endregion ////
+  /**
+   * Unsubscribe from the onMessage event of the parent window
+   */
+  unsubscribe(): void {
+    window.removeEventListener('message', this.runCallbackHandler)
+  }
 
-	/**
-	 * Send message to parent window
-	 * The answer (if) we will get in _runCallback
-	 *
-	 * @param command
-	 * @param params
-	 */
-	async send(
-		command: string | MessageCommands,
-		params: null | SendParams = null
-	): Promise<any> {
-		return new Promise((resolve, reject) => {
-			let cmd: string | object
-			const promiseHandler: PromiseHandlers = {
-				resolve,
-				reject,
-				timeoutId: null,
-			}
+  // endregion ////
 
-			const keyPromise = this.#setCallbackPromise(promiseHandler)
+  /**
+   * Send message to parent window
+   * The answer (if) we will get in _runCallback
+   *
+   * @param command
+   * @param params
+   */
+  async send(
+    command: string | MessageCommands,
+    params: null | SendParams = null
+  ): Promise<any> {
+    return new Promise((resolve, reject) => {
+      let cmd: string | object
+      const promiseHandler: PromiseHandlers = {
+        resolve,
+        reject,
+        timeoutId: null,
+      }
 
-			if (command.toString().includes(':')) {
-				cmd = {
-					method: command.toString(),
-					params: params ?? '',
-					callback: keyPromise,
-					appSid: this.#appFrame.getAppSid(),
-				}
-			} else {
-				/**
-				 * @memo: delete it after rest 22.0.0
-				 */
-				cmd = command.toString()
-				const listParams = [
-					params ? JSON.stringify(params) : null,
-					keyPromise,
-					this.#appFrame.getAppSid(),
-				]
+      const keyPromise = this.#setCallbackPromise(promiseHandler)
 
-				cmd += ':' + listParams.filter(Boolean).join(':')
-			}
+      const paramsSend = omit(params || {}, ['callBack', 'isSafely', 'safelyTime'])
+      const { callBack } = params || {}
+      if (callBack) {
+        this.#callbackSingletone.set(keyPromise, callBack)
+      }
 
-			this.getLogger().log(`send to ${this.#appFrame.getTargetOrigin()}`, {
-				cmd,
-			})
+      if (command.toString().includes(':')) {
+        cmd = {
+          method: command.toString(),
+          params: paramsSend ?? '',
+          callback: keyPromise,
+          appSid: this.#appFrame.getAppSid(),
+        }
+      } else {
+        /**
+         * @memo: delete it after rest 22.0.0
+         */
+        cmd = command.toString()
+        const listParams = [
+          params ? JSON.stringify(paramsSend) : null,
+          keyPromise,
+          this.#appFrame.getAppSid(),
+        ]
 
-			parent.postMessage(cmd, this.#appFrame.getTargetOrigin())
+        cmd += ':' + listParams.filter(Boolean).join(':')
+      }
 
-			if (params?.isSafely) {
-				// @ts-ignore
-				this.#callbackPromises.get(keyPromise).timeoutId = window.setTimeout(
-					() => {
-						if (this.#callbackPromises.has(keyPromise)) {
-							this.getLogger().warn(
-								`Action ${command.toString()} stop by timeout`
-							)
+      this.getLogger().log(`send to ${ this.#appFrame.getTargetOrigin() }`, {
+        cmd,
+      })
 
-							this.#callbackPromises.delete(keyPromise)
-							resolve({ isSafely: true })
-						}
-					},
-					Number.parseInt(String(params?.safelyTime || 900))
-				)
-			}
-		})
-	}
+      parent.postMessage(cmd, this.#appFrame.getTargetOrigin())
 
-	/**
-	 * Fulfilling a promise based on messages from the parent window
-	 *
-	 * @param event
-	 * @private
-	 */
-	public _runCallback(event: MessageEvent): void {
-		if (event.origin !== this.#appFrame.getTargetOrigin()) {
-			return
-		}
+      if (params?.isSafely) {
+        // @ts-ignore
+        this.#callbackPromises.get(keyPromise).timeoutId = window.setTimeout(
+          () => {
+            if (this.#callbackPromises.has(keyPromise)) {
+              this.getLogger().warn(
+                `Action ${ command.toString() } stop by timeout`
+              )
 
-		if (event.data) {
-			this.getLogger().log(`get from ${event.origin}`, {
-				data: event.data,
-			})
+              this.#callbackPromises.delete(keyPromise)
+              resolve({ isSafely: true })
+            }
+          },
+          Number.parseInt(String(params?.safelyTime || 900))
+        )
+      }
+    })
+  }
 
-			const tmp = event.data.split(':')
+  /**
+   * Fulfilling a promise based on messages from the parent window
+   *
+   * @param event
+   * @private
+   */
+  public _runCallback(event: MessageEvent): void {
+    if (event.origin !== this.#appFrame.getTargetOrigin()) {
+      return
+    }
 
-			const cmd: { id: string; args: any } = {
-				id: tmp[0],
-				args: tmp.slice(1).join(':'),
-			}
+    if (event.data) {
+      this.getLogger().log(`get from ${ event.origin }`, {
+        data: event.data,
+      })
 
-			if (cmd.args) {
-				cmd.args = JSON.parse(cmd.args)
-			}
+      const tmp = event.data.split(':')
 
-			if (this.#callbackPromises.has(cmd.id)) {
-				const promise = this.#callbackPromises.get(cmd.id) as PromiseHandlers
-				if (promise.timeoutId) {
-					clearTimeout(promise.timeoutId)
-				}
+      const cmd: { id: string; args: any } = {
+        id: tmp[0],
+        args: tmp.slice(1).join(':'),
+      }
 
-				this.#callbackPromises.delete(cmd.id)
+      if (cmd.args) {
+        cmd.args = JSON.parse(cmd.args)
+      }
 
-				promise.resolve(cmd.args)
-				// promise.reject(cmd.args) ////
-			}
-		}
-	}
+      if (this.#callbackPromises.has(cmd.id)) {
+        const promise = this.#callbackPromises.get(cmd.id) as PromiseHandlers
+        if (promise.timeoutId) {
+          clearTimeout(promise.timeoutId)
+        }
 
-	/**
-	 * Storing a promise for a message from the parent window
-	 *
-	 * @param promiseHandler
-	 * @private
-	 *
-	 * @memo We don't use Symbol here, because we need to pass it to the parent and then find and restore it.
-	 */
-	#setCallbackPromise(promiseHandler: PromiseHandlers): string {
-		const key = Text.getUniqId()
-		this.#callbackPromises.set(key, promiseHandler)
-		return key
-	}
+        this.#callbackPromises.delete(cmd.id)
+
+        promise.resolve(cmd.args)
+        // promise.reject(cmd.args) ////
+      } else if (this.#callbackSingletone.has(cmd.id)) {
+        const callBack = this.#callbackSingletone.get(cmd.id) as (...args: any[]) => void
+        if (callBack) {
+          callBack.apply(globalThis, [cmd.args])
+        }
+      }
+    }
+  }
+
+  /**
+   * Storing a promise for a message from the parent window
+   *
+   * @param promiseHandler
+   * @private
+   *
+   * @memo We don't use Symbol here, because we need to pass it to the parent and then find and restore it.
+   */
+  #setCallbackPromise(promiseHandler: PromiseHandlers): string {
+    const key = Text.getUniqId()
+    this.#callbackPromises.set(key, promiseHandler)
+    return key
+  }
 }
--- packages/jssdk/src/frame/placement.ts
+++ packages/jssdk/src/frame/placement.ts
@@ -8,94 +8,96 @@ import type { MessageInitData } from '../types/auth'
  * @see https://dev.1c-bitrix.ru/learning/course/index.php?COURSE_ID=99&CHAPTER_ID=02535&LESSON_PATH=8771.5380.2535
  */
 export class PlacementManager {
-	#messageManager: MessageManager
-	#title: string = ''
-	#options: object = {}
+  #messageManager: MessageManager
+  #title: string = ''
+  #options: object = {}
 
-	constructor(messageManager: MessageManager) {
-		this.#messageManager = messageManager
-	}
+  constructor(messageManager: MessageManager) {
+    this.#messageManager = messageManager
+  }
 
-	/**
-	 * Initializes the data received from the parent window message.
-	 * @param data
-	 */
-	initData(data: MessageInitData): PlacementManager {
-		this.#title = data.PLACEMENT || 'DEFAULT'
-		const options = data.PLACEMENT_OPTIONS
+  /**
+   * Initializes the data received from the parent window message.
+   * @param data
+   */
+  initData(data: MessageInitData): PlacementManager {
+    this.#title = data.PLACEMENT || 'DEFAULT'
+    const options = data.PLACEMENT_OPTIONS
 
-		this.#options = Object.freeze(options)
+    this.#options = Object.freeze(options)
 
-		return this
-	}
+    return this
+  }
 
-	get title(): string {
-		return this.#title
-	}
+  get title(): string {
+    return this.#title
+  }
 
-	get isDefault(): boolean {
-		return this.title === 'DEFAULT'
-	}
+  get isDefault(): boolean {
+    return this.title === 'DEFAULT'
+  }
 
-	get options(): any {
-		return this.#options
-	}
+  get options(): any {
+    return this.#options
+  }
 
-	get isSliderMode(): boolean {
-		return this.options?.IFRAME === 'Y'
-	}
+  get isSliderMode(): boolean {
+    return this.options?.IFRAME === 'Y'
+  }
 
-	/**
-	 * Get Information About the JS Interface of the Current Embedding Location
-	 *
-	 * @return {Promise<any>}
-	 *
-	 * @link https://apidocs.bitrix24.com/api-reference/widgets/ui-interaction/bx24-placement-get-interface.html
-	 */
-	async getInterface(): Promise<any>
-	{
-		return this.#messageManager.send(
-			MessageCommands.getInterface,
-			{
-				isSafely: true
-			}
-		)
-	}
+  /**
+   * Get Information About the JS Interface of the Current Embedding Location
+   *
+   * @return {Promise<any>}
+   *
+   * @link https://apidocs.bitrix24.com/api-reference/widgets/ui-interaction/bx24-placement-get-interface.html
+   */
+  async getInterface(): Promise<any> {
+    return this.#messageManager.send(
+      MessageCommands.getInterface,
+      {
+        isSafely: true
+      }
+    )
+  }
 
-	/**
-	 * Set Up the Interface Event Handler
-	 * @param {string} eventName
-	 * @return {Promise<any>}
-	 *
-	 * @link https://apidocs.bitrix24.com/api-reference/widgets/ui-interaction/bx24-placement-bind-event.html
-	 */
-	async bindEvent(eventName: string): Promise<any>
-	{
-		return this.#messageManager.send(
-			MessageCommands.getInterface,
-			{
-				event: eventName,
-				isSafely: true
-			}
-		)
-	}
-	
-	/**
-	 * Call the Registered Interface Command
-	 * @param {string} command
-	 * @param {Record<string, any>} parameters
-	 * @return {Promise<any>}
-	 *
-	 * @link https://apidocs.bitrix24.com/api-reference/widgets/ui-interaction/bx24-placement-call.html
-	 */
-	async call(command: string, parameters: Record<string, any> = {}): Promise<any>
-	{
-		return this.#messageManager.send(
-			command,
-			{
-				...parameters,
-				isSafely: true
-			}
-		)
-	}
+  /**
+   * Set Up the Interface Event Handler
+   * @param {string} eventName
+   * @param {(...args: any[]) => void} callBack
+   * @return {Promise<any>}
+   *
+   * @link https://apidocs.bitrix24.com/api-reference/widgets/ui-interaction/bx24-placement-bind-event.html
+   */
+  async bindEvent(
+    eventName: string,
+    callBack: (...args: any[]) => void
+  ): Promise<any> {
+    return this.#messageManager.send(
+      MessageCommands.placementBindEvent,
+      {
+        event: eventName,
+        callBack,
+        isSafely: true
+      }
+    )
+  }
+
+  /**
+   * Call the Registered Interface Command
+   * @param {string} command
+   * @param {Record<string, any>} parameters
+   * @return {Promise<any>}
+   *
+   * @link https://apidocs.bitrix24.com/api-reference/widgets/ui-interaction/bx24-placement-call.html
+   */
+  async call(command: string, parameters: Record<string, any> = {}): Promise<any> {
+    return this.#messageManager.send(
+      command,
+      {
+        ...parameters,
+        isSafely: true
+      }
+    )
+  }
 }

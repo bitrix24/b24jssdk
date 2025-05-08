555cc43315aa3a8ad5e6f414997a7ee054edaffb
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-08 17:12:54 +0300

    chore(enums): helper getEnumValue

--- packages/jssdk/src/tools/index.ts
+++ packages/jssdk/src/tools/index.ts
@@ -1,3 +1,6 @@
+/**
+ * @todo add docs
+ */
 export function pick<Data extends object, Keys extends keyof Data>(data: Data, keys: Keys[]): Pick<Data, Keys> {
   const result = {} as Pick<Data, Keys>
 
@@ -8,17 +11,38 @@ export function pick<Data extends object, Keys extends keyof Data>(data: Data, k
   return result
 }
 
+/**
+ * @todo add docs
+ */
 export function omit<Data extends object, Keys extends keyof Data>(data: Data, keys: Keys[]): Omit<Data, Keys> {
   const result = { ...data }
 
   for (const key of keys) {
-     
+
     delete result[key]
   }
 
   return result as Omit<Data, Keys>
 }
 
+/**
+ * @todo add docs
+ */
 export function isArrayOfArray<A>(item: A[] | A[][]): item is A[][] {
   return Array.isArray(item[0])
+}
+
+/**
+ * @todo add docs
+ *
+ * @example
+ * const result = getEnumValue(EnumBizprocDocumentType, 'CCrmDocumentSmartOrder')
+ */
+export function getEnumValue<T extends Record<string, string | number>>(
+  enumObj: T,
+  value: string | number
+): T[keyof T] | undefined {
+  return (Object.values(enumObj) as (string | number)[]).includes(value)
+    ? value as T[keyof T]
+    : undefined
 }

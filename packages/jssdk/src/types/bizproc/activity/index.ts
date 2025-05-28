a8622461de0768dad2de5d5f474a7bbd26402db3
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-28 09:11:56 +0300

    feat(types/bizproc/activity): add some type for `bizproc.activity`

--- packages/jssdk/src/types/bizproc/activity/index.ts
+++ packages/jssdk/src/types/bizproc/activity/index.ts
@@ -7,6 +7,7 @@
  */
 import type { BoolString } from '../../common'
 import type { HandlerAuthParams } from '../../handler'
+import type { B24LangList } from '../../../core/language/list'
 
 export interface ActivityHandlerParams {
   event_token: string
@@ -18,5 +19,43 @@ export interface ActivityHandlerParams {
   use_subscription: BoolString
   timeout_duration: string
   ts: string
+  [key: string]: any
   auth: HandlerAuthParams
+}
+
+export type ActivityPropertyType = 'bool' | 'date' | 'datetime' | 'double' | 'int' | 'select' | 'string' | 'text' | 'user'
+
+export interface ActivityProperty {
+  Name: string | Partial<Record<B24LangList, string>>
+  Description?: string | Record<string, string>
+  Type: ActivityPropertyType
+  Options?: Record<string | number, string>
+  Required?: BoolString
+  Multiple?: BoolString
+  Default?: any
+}
+
+export interface ActivityConfig {
+  CODE: string
+  HANDLER: string
+  NAME: string | Partial<Record<B24LangList, string>>
+  DESCRIPTION?: string | Partial<Record<B24LangList, string>>
+  DOCUMENT_TYPE?: [string, string, string]
+  PROPERTIES?: Record<string, ActivityProperty>
+  RETURN_PROPERTIES?: Record<string, ActivityProperty>
+  FILTER?: {
+    INCLUDE?: Array<string | string[]>
+    EXCLUDE?: Array<string | string[]>
+  }
+  USE_PLACEMENT?: BoolString
+  PLACEMENT_HANDLER?: string
+  USE_SUBSCRIPTION?: BoolString
+  AUTH_USER_ID?: number
+}
+
+export interface ActivityOrRobotConfig extends Omit<ActivityConfig, 'HANDLER' | 'PLACEMENT_HANDLER' | 'NAME'> {
+  type: 'activity' | 'robot'
+  NAME?: ActivityConfig['NAME']
+  HANDLER?: ActivityConfig['HANDLER']
+  PLACEMENT_HANDLER?: ActivityConfig['PLACEMENT_HANDLER']
 }

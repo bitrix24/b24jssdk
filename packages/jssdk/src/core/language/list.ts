69a8b84fcd57627e97e7ff1c9f4bae5475bad517
Shevchik Igor <s.hevchik.mail@gmail.com>
2025-05-28 09:10:25 +0300

    feat(B24LocaleMap): add map for B24LangList and Locale

--- packages/jssdk/src/core/language/list.ts
+++ packages/jssdk/src/core/language/list.ts
@@ -28,3 +28,27 @@ export enum B24LangList {
 	th = 'th',
 	ar = 'ar',
 }
+
+/**
+ * @todo add docs
+ */
+export const B24LocaleMap: Record<B24LangList, string> = {
+  [B24LangList.en]: 'en-EN',
+  [B24LangList.de]: 'de-DE',
+  [B24LangList.la]: 'es-ES',
+  [B24LangList.br]: 'pt-BR',
+  [B24LangList.fr]: 'fr-FR',
+  [B24LangList.it]: 'it-IT',
+  [B24LangList.pl]: 'pl-PL',
+  [B24LangList.ru]: 'ru-RU',
+  [B24LangList.ua]: 'uk-UA',
+  [B24LangList.tr]: 'tr-TR',
+  [B24LangList.sc]: 'zh-CN',
+  [B24LangList.tc]: 'zh-TW',
+  [B24LangList.ja]: 'ja-JP',
+  [B24LangList.vn]: 'vi-VN',
+  [B24LangList.id]: 'id-ID',
+  [B24LangList.ms]: 'ms-MY',
+  [B24LangList.th]: 'th-TH',
+  [B24LangList.ar]: 'ar-SA'
+}

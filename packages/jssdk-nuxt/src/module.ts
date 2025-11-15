bc42d5d8258c1b6a64677114746719d89fd1b171
IgorShevchik <s.hevchik.mail@gmail.com>
2025-11-15 10:14:45 +0300

    chore(packages\jssdk-nuxt): init

---
+++ packages/jssdk-nuxt/src/module.ts
@@ -0,0 +1,20 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+
+export type ModuleOptions = object
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: '@bitrix24/b24jssdk-nuxt',
+    version: '0.5.1',
+    configKey: 'B24JsSdkNuxt',
+    compatibility: {
+      nuxt: '>=4.0.0'
+    }
+  },
+  defaults: {},
+  async setup(_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    addPlugin({ src: resolver.resolve('./runtime/plugin') })
+  }
+})

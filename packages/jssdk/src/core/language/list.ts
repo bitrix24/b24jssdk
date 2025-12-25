969b09fd46fa08543bce83da43c802272cb3eca9
IgorShevchik <s.hevchik.mail@gmail.com>
2025-12-25 07:35:13 +0300

    feat(languages): add kz and in

--- packages/jssdk/src/core/language/list.ts
+++ packages/jssdk/src/core/language/list.ts
@@ -4,51 +4,59 @@
  * It is worth remembering that there will be 1-2 languages for the B24.Box
  */
 export enum B24LangList {
-  en = 'en',
+  ru = 'ru',
+  id = 'id',
+  ms = 'ms',
+
   de = 'de',
+  en = 'en',
   la = 'la',
 
-  br = 'br',
   fr = 'fr',
+  in = 'in',
   it = 'it',
 
   pl = 'pl',
-  ru = 'ru',
-  ua = 'ua',
+  br = 'br',
+  vn = 'vn',
 
   tr = 'tr',
-  sc = 'sc',
-  tc = 'tc',
+  kz = 'kz',
+  ua = 'ua',
 
-  ja = 'ja',
-  vn = 'vn',
-  id = 'id',
-
-  ms = 'ms',
+  ar = 'ar',
   th = 'th',
-  ar = 'ar'
+  sc = 'sc',
+
+  tc = 'tc',
+  ja = 'ja'
 }
 
-/**
- * @todo add docs
- */
 export const B24LocaleMap: Record<B24LangList, string> = {
-  [B24LangList.en]: 'en-EN',
-  [B24LangList.de]: 'de-DE',
-  [B24LangList.la]: 'es-ES',
-  [B24LangList.br]: 'pt-BR',
-  [B24LangList.fr]: 'fr-FR',
-  [B24LangList.it]: 'it-IT',
-  [B24LangList.pl]: 'pl-PL',
   [B24LangList.ru]: 'ru-RU',
-  [B24LangList.ua]: 'uk-UA',
-  [B24LangList.tr]: 'tr-TR',
-  [B24LangList.sc]: 'zh-CN',
-  [B24LangList.tc]: 'zh-TW',
-  [B24LangList.ja]: 'ja-JP',
-  [B24LangList.vn]: 'vi-VN',
   [B24LangList.id]: 'id-ID',
   [B24LangList.ms]: 'ms-MY',
+
+  [B24LangList.de]: 'de-DE',
+  [B24LangList.en]: 'en-EN',
+  [B24LangList.la]: 'es-ES',
+
+  [B24LangList.fr]: 'fr-FR',
+  [B24LangList.in]: 'hi-IN',
+  [B24LangList.it]: 'it-IT',
+
+  [B24LangList.pl]: 'pl-PL',
+  [B24LangList.br]: 'pt-BR',
+  [B24LangList.vn]: 'vi-VN',
+
+  [B24LangList.tr]: 'tr-TR',
+  [B24LangList.kz]: 'kk',
+  [B24LangList.ua]: 'uk-UA',
+
+  [B24LangList.ar]: 'ar-SA',
   [B24LangList.th]: 'th-TH',
-  [B24LangList.ar]: 'ar-SA'
+  [B24LangList.sc]: 'zh-CN',
+
+  [B24LangList.tc]: 'zh-TW',
+  [B24LangList.ja]: 'ja-JP'
 }

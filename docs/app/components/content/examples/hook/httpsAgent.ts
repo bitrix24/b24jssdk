557fb8f58ce2920d1974f8b46bb2d31ee83c0d88
IgorShevchik <s.hevchik.mail@gmail.com>
2026-02-02 10:20:01 +0300

    chore(docs): improve

--- docs/app/components/content/examples/hook/httpsAgent.ts
+++
@@ -1,9 +0,0 @@
-import { B24Hook, ApiVersion } from '@bitrix24/b24jssdk'
-import * as http from 'node:http'
-
-const $b24 = B24Hook.fromWebhookUrl('https://your_domain.bitrix24.com/rest/1/webhook_code/')
-
-const axiosClient = $b24.getHttpClient(ApiVersion.v2).ajaxClient
-axiosClient.default.httpAgent = new http.Agent({ keepAlive: false })
-axiosClient.default.httpsAgent = new http.Agent({ keepAlive: false })
-axiosClient.default.timeout = 300_000
